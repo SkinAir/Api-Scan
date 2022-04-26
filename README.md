517f79ded108c84850763e191cd780e1548d68a2
SkinAir <71880910+SkinAir@users.noreply.github.com>
2022-04-26 22:36:13 +0800

    1

---
+++ README.md
@@ -0,0 +1,24 @@
+### 0x01：路由知识
+
+- 有些程序员会自定义 `/manage`、`/management` 、**项目 App 相关名称**为 spring 根路径
+- Spring Boot Actuator 1.x 版本默认内置路由的起始路径为 `/` ，2.x 版本则统一以 `/actuator` 为起始路径
+- Spring Boot Actuator 默认的内置路由名字，如 `/env` 有时候也会被程序员修改，比如修改成 `/appenv`
+
+### 0x02：版本知识
+
+> Spring Cloud 是基于 Spring Boot 来进行构建服务，并提供如配置管理、服务注册与发现、智能路由等常见功能的帮助快速开发分布式系统的系列框架的有序集合。
+
+#### 
+
+## 扫描常见Api
+
+使用方法
+
+```python
+python3 api.py urls.txt
+```
+
+理论参考：
+
+https://github.com/LandGrey/SpringBootVulExploit
+
---
+++ api.py
@@ -0,0 +1,70 @@
+
+#!/usr/bin/env python
+# coding:utf-8
+
+from cgi import test
+from importlib.resources import path
+from urllib import response
+import requests,sys,colorama
+from colorama import *
+import sys
+
+banner = '''\033[1;33;40m
+   _____ __________.___ 
+  /  _  \\______   \   |
+ /  /_\  \|     ___/   |
+/    |    \    |   |   |
+\____|__  /____|   |___|
+        \/              
+Have fun. Don't forget t
+'''
+
+def examples():
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36"}
+    with open(r'C:\Users\28564\Desktop\python-exp\ms-T\Exp.txt','r')as f:
+        payload = f.readlines()
+    # payload = '/swagger/index.html'
+    # poc = urls + payload
+    try:
+        requests.packages.urllib3.disable_warnings()
+        for exp in payload:
+            exp = exp.strip('\n')
+            response = requests.get(urls+exp, headers=headers, timeout=15,verify=False)
+            if response.status_code == 200:
+            #if response.status_code == 200:
+                print(u'\033[1;31;40m[+]{} is may exists'.format(urls))
+                print(response.text)
+                with open('./APIresult.txt','a') as f:
+                    f.write('[+]' + urls + exp)
+                    f.write('\n')
+            else:
+                print('\033[1;32;40m[-]None: {}'.format(urls)+ exp)
+    # except:
+    #     print('{} request timeout'.format(urls))
+    except Exception as e:
+        print(e)
+
+if __name__ == '__main__':
+    try:
+        print(banner)
+        path=sys.argv[1]
+        if len(sys.argv) != 2:
+            print('Example:python3 api.py urls.txt')
+        else:
+            with open(path , 'r') as u:
+                line = u.readlines()
+                for url in line:
+                    urls = url.strip()
+                    if urls[-1] == '/':
+                        urls = urls[:-1]
+                    examples()
+                print('over')
+    except Exception as e:
+        print(e)
+    # except:
+    #     print('python3 api.py url.txt')
+    #     sys.exit()
+
+
+                
+
